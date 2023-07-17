db1992ea26f221b31b855e6ceb698156785156f2
ErnestKnowing <e3050901@gmail.com>
2023-07-17 11:51:40 +0300

    feat: dimmed the background around the modal window

--- js/script_modal_window.js
+++ js/script_modal_window.js
@@ -1,7 +1,9 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+    document.getElementById('myFon').style.display = "block";
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+    document.getElementById('myFon').style.display = "none";
   }
